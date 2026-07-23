28793abe3eb5128429bcb839c73417e2ab3ea274
NilCore <nilcore5060@gmail.com>
2026-07-23 09:55:02 +0300

    Update Crypto.go

--- golang/payments/methods/Crypto.go
+++ golang/payments/methods/Crypto.go
@@ -12,12 +12,12 @@ func NewCrypto() Crypto {
 }
 
 func (c Crypto) Pay(usd int) int {
-	fmt.Println("оплата криптой")
+	fmt.Println("оплата криптовалютой")
 	fmt.Println("сумма:", usd, "usdt")
 
 	return rand.Int()
 }
 
 func (c Crypto) Cancel(id int) {
-	fmt.Println("отмена крипто опирация id=", id)
+	fmt.Println("отмена крипто операции id=", id)
 }
