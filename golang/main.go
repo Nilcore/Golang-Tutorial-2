c7d123a6ad3879656dca5102877b353cbff4c40f
NilCore <nilcore5060@gmail.com>
2026-07-23 09:57:18 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -13,12 +13,12 @@ func main() {
 
 	paymentModule.Pay("бургер", 5)
 	IdPhone := paymentModule.Pay("телефон", 500)
-	IdGame := paymentModule.Pay("стим", 20)
+	IdGame := paymentModule.Pay("игра", 20)
 
 	paymentModule.Cancel(IdPhone)
 	gameinfo := paymentModule.Info(IdGame)
 	allinfo := paymentModule.AllInfo()
 
 	fmt.Println("все наши оплаты", allinfo)
-	fmt.Println("гейм инфо", gameinfo)
+	fmt.Println("игра info", gameinfo)
 }
